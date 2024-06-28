ee8de6b41b655d460387a452e1a4cb2fc47983b6
Hossam Medhat <125540523+Hskaa@users.noreply.github.com>
2024-06-28 21:15:03 +0300

    change 1 in lab1

--- C/Lab_01/lab1.c
+++ C/Lab_01/lab1.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
 int main(){
-	int x;
-	printf("Please enter number:");
-	scanf("%d",&x);
-	if(x%2==0){
-		printf("number is even");
-	}
-	else{
-		printf("number is odd");
-	}
+
 	return 0;
 	
 }
